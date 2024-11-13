92ea0ebde33fb1af7c8d11db6cadfd50d6cd80bb
brian <brianmstricker@gmail.com>
2024-11-13 17:30:52 -0500

    chore: updated manifest/vite build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
-})
+ plugins: [react()],
+ build: {
+  outDir: "dist",
+  rollupOptions: {
+   input: {
+    index: resolve(__dirname, "index.html"),
+    background: resolve(__dirname, "src/background.ts"),
+   },
+   output: {
+    entryFileNames: "[name].js",
+   },
+  },
+ },
+});
